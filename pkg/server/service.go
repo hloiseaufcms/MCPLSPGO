1b394fe60bb16dd0adc383b057e43467a8a54c97
Barton Rhodes <barton@bmorphism.xyz>
2025-04-09 05:49:25 -0700

    Update service.go to fix the API for main.go

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -1,11 +1,10 @@
 package server
 
-// This file provides the expected API for main.go
+// This file provides the expected API for main.go by acting as an
+// adapter to the implementations in server.go
 
 // NewService creates a new MCP service for gopls integration
 func NewService() (*Service, error) {
-	// We're reusing the existing Service struct and its creation logic
-	// from server.go
 	logFile, err := setupLogger()
 	if err != nil {
 		return nil, err
